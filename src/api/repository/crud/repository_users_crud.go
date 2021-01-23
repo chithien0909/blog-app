1154426ac0185f5bc02fa77a7e9c420f359b2e54
chithien0909 <chithien0909@gmail.com>
2021-01-23 11:35:46 +0700

    feature: delte user by id api

--- src/api/repository/crud/repository_users_crud.go
+++ src/api/repository/crud/repository_users_crud.go
@@ -5,6 +5,7 @@ import (
 	"../../utils/channels"
 	"errors"
 	"gorm.io/gorm"
+	"time"
 )
 
 type repositoryUsersCRUD struct {
@@ -37,6 +38,7 @@ func (r *repositoryUsersCRUD) FindAll() ([]models.User, error) {
 	done := make(chan bool)
 	var users []models.User
 	go func(ch chan<- bool) {
+		defer close(ch)
 		err = r.db.Debug().Model(&models.User{}).Limit(100).Find(&users).Error
 		if err != nil {
 			ch <- false
@@ -54,6 +56,7 @@ func (r *repositoryUsersCRUD) FindById(uid uint64) (models.User, error) {
 	done := make(chan bool)
 	var user models.User
 	go func(ch chan<- bool) {
+		defer close(ch)
 		err = r.db.Debug().Model(&models.User{}).Where("id = ?", uid).Take(&user).Error
 		if err != nil {
 			ch <- false
@@ -68,4 +71,44 @@ func (r *repositoryUsersCRUD) FindById(uid uint64) (models.User, error) {
 		return models.User{}, errors.New("user not found")
 	}
 	return models.User{}, err
+}
+
+func (r *repositoryUsersCRUD) Update(uid uint64, user models.User) (int64, error) {
+	var rs  *gorm.DB
+	done := make(chan bool)
+	go func(ch chan<- bool) {
+		defer close(ch)
+		rs = r.db.Debug().Model(&models.User{}).Where("id = ?", uid).UpdateColumns(
+			map[string]interface{}{
+				"nickname": user.Nickname,
+				"email": user.Email,
+				"updated_at": time.Now(),
+			},
+		)
+		ch <- true
+	}(done)
+	if channels.OK(done) {
+		if  rs.Error != nil {
+			return 0, rs.Error
+		}
+		return rs.RowsAffected, nil
+	}
+	return 0, rs.Error
+}
+
+func (r *repositoryUsersCRUD) Delete(uid uint64) (int64, error) {
+	var rs  *gorm.DB
+	done := make(chan bool)
+	go func(ch chan<- bool) {
+		defer close(ch)
+		rs = r.db.Debug().Model(&models.User{}).Where("id = ?", uid).Delete(&models.User{})
+		ch <- true
+	}(done)
+	if channels.OK(done) {
+		if  rs.Error != nil {
+			return 0, rs.Error
+		}
+		return rs.RowsAffected, nil
+	}
+	return 0, rs.Error
 }
